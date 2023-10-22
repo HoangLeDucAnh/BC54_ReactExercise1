0c6b186bcccce7ddd165d7cf76a4abe423a46ad7
Anhtony201 <hoangleducanh123@gmail.com>
2023-10-22 12:15:07 +0700

    update

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import logo from "./logo.svg";
+import "./App.css";
+import NewLayout from "./Ex_1/NewLayout";
+function App() {
+  return (
+    <div>
+      <NewLayout />
+    </div>
+  );
+}
+
+export default App;
